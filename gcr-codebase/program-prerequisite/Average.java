0c7c30a685ceeec0b7110c6067fd2da34411f40f
Sanvi <kumarisanvi2511@gmail.com>
2025-12-16 23:43:11 +0530

    [Sanvi] : Added program of Average of three numbers

---
+++ gcr-codebase/program-prerequisite/Average.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Average{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+double a=sc.nextDouble();
+double b=sc.nextDouble();
+double c=sc.nextDouble();
+double avg=(a+b+c)/3;
+System.out.println(avg);
+}
+}

326995e44b0d4df9e71d88a67f2e781beac4ddfa
Sanvi <kumarisanvi2511@gmail.com>
2025-12-16 22:25:10 +0530

    [Sanvi] : Added program of Simple Interest

---
+++ gcr-codebase/program-prerequisite/SimpleInterest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SimpleInterest{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+double p=sc.nextDouble();
+double r=sc.nextDouble();
+double t=sc.nextDouble();
+double SI=(p*r*t)/100;
+System.out.println(SI);
+}
+}

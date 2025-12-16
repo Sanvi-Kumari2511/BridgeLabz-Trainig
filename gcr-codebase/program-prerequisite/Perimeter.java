5b7ca9a469b660d702673deaeea9d39e0353765c
Sanvi <kumarisanvi2511@gmail.com>
2025-12-16 22:44:47 +0530

    [Sanvi] : Added program of perimeter of Rectangle

---
+++ gcr-codebase/program-prerequisite/Perimeter.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Perimeter{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+double l=sc.nextDouble();
+double b=sc.nextDouble();
+double perimeter=2*(l+b);
+System.out.println(perimeter);
+}
+}

ef722f5ff1bd1a9bde33ee08a7bb39741e4a04bd
Sanvi <kumarisanvi2511@gmail.com>
2025-12-16 17:09:43 +0530

    [Sanvi] : Added program of Volume of Cylinder

---
+++ gcr-codebase/program-prerequisite/CylinderVolume.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class CylinderVolume{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+double rad=sc.nextDouble();
+double h=sc.nextDouble();
+double vol=Math.PI*rad*rad*h;
+System.out.println(vol);
+
+}
+}

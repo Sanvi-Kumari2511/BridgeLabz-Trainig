fcdd43b953aecfe2e99ef5b83ad418a60588eb57
Sanvi <kumarisanvi2511@gmail.com>
2025-12-17 22:38:15 +0530

    [Sanvi] : Added Sam's marks program

---
+++ gcr-codebase/programming-elements/Level1/SamMarks.java
@@ -0,0 +1,11 @@
+public class SamMarks
+{
+ public static void  main(String args [])
+   {
+    int maths = 94;
+    int physics = 95;
+    int chemistry = 96;
+    int average = (maths+physics+chemistry)/3;
+    System.out.println("Sam's average marks in PCM is " + average);
+   }
+}

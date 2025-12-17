e2ebebc088e1a0fafe5a136598d78c8daa2a4c90
Sanvi <kumarisanvi2511@gmail.com>
2025-12-17 22:51:03 +0530

    [Sanvi] : Added quantity of pens program

---
+++ gcr-codebase/programming-elements/Level1/PensQuantity.java
@@ -0,0 +1,11 @@
+public class PensQuantity
+{
+ public static void  main(String args [])
+   {
+    int pens = 14;
+    int students = 3;
+    int TotalPensQuantity = 14/3;
+    int remainingPens = 14%3;
+    System.out.println("The Pen Per Student is " + TotalPensQuantity + " and the remaining pen not distributed is " + remainingPens);
+   }
+}

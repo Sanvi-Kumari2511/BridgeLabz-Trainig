f564bc0de04ffa2fe08dac41d5fffee5d639f8dd
Sanvi <kumarisanvi2511@gmail.com>
2025-12-17 22:45:57 +0530

    [Sanvi] : Added Conversion program of km to miles

---
+++ gcr-codebase/programming-elements/Level1/KmToMiles.java
@@ -0,0 +1,16 @@
+//Converting Kilometers to Miles
+public class KmToMiles
+{
+       public static void main(String[] args)
+       {
+		
+		//Giving Inputs
+		double distanceInKilometers = 10.8;
+
+		//Calculating Distance in Miles
+		double distanceInMiles = distanceInKilometers/1.6;
+
+		//Displaying outputs
+		System.out.println("The distance " + distanceInKilometers + " km in miles is " + distanceInMiles);
+	}
+  }

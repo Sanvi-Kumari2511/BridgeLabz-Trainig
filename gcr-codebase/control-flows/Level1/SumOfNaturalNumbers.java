2a884c0244076f81527ebfb1915930bee62e6a51
Sanvi <kumarisanvi2511@gmail.com>
2025-12-19 15:45:41 +0530

    [Sanvi] : Added sum of natural numbers program

---
+++ gcr-codebase/control-flows/Level1/SumOfNaturalNumbers.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+// program to find the sum of numbers until the user enters 0 using while loop and break statement.
+
+public class SumOfNaturalNumbers
+{
+       public static void main(String args[])
+           {
+            Scanner sc=new Scanner(System.in);
+              
+             // Taking user input
+             int number = sc.nextInt();
+             if(number>0)
+            {
+            
+            // Calculating sum of natural numbers
+            int formulaBasedSum = (number*(number+1))/2;
+            int i = number;
+            int loopBasedSum = 0;
+             while(i>0)
+             {
+              loopBasedSum +=i;
+              i--;
+              }
+               // printing the sum of natural numbers
+               System.out.println("The sum of  natural numbers using formula " + formulaBasedSum + " and " + " the sum of  natural numbers using loop " + loopBasedSum);
+              }              
+              else{
+               System.out.println("Number " + number + " is not natural number");
+               }
+              //closing the scanner 
+               sc.close();
+            }
+     }

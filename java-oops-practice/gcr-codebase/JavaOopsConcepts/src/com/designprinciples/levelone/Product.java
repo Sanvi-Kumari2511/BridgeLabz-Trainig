cacb0f6a5a14ca05803831741b4eb8af34bce832
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 01:09:57 +0530

    [Sanvi] : Added Product class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/designprinciples/levelone/Product.java
@@ -0,0 +1,25 @@
+package com.designprinciples.levelone;
+
+public class Product {
+	
+	    private int productId;
+	    private String productName;
+	    private double price;
+
+	    public Product(int productId, String productName, double price) {
+	        this.productId = productId;
+	        this.productName = productName;
+	        this.price = price;
+	    }
+
+	    public double getPrice() {
+	        return price;
+	    }
+
+	    public void displayProduct() {
+	        System.out.println("Product ID: " + productId +
+	                           ", Name: " + productName +
+	                           ", Price: ₹" + price);
+	    }
+	}
+

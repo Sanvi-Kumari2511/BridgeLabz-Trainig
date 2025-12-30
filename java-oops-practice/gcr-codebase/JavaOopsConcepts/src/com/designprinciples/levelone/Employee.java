8ad7e06104ed81ce6628ca1922296812c8ec558c
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 01:21:06 +0530

    [Sanvi] : Added Employee class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/designprinciples/levelone/Employee.java
@@ -0,0 +1,17 @@
+package com.designprinciples.levelone;
+
+public class Employee {
+
+	    private int empId;
+	    private String name;
+
+	    public Employee(int empId, String name) {
+	        this.empId = empId;
+	        this.name = name;
+	    }
+
+	    public void displayEmployee() {
+	        System.out.println("Employee ID: " + empId + ", Name: " + name);
+	    }
+	}
+

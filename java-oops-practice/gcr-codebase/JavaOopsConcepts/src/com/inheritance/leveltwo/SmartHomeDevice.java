391f2a6ccc6a34e76ede9552c15ad161d62d53d7
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 20:06:23 +0530

    [Sanvi] : Added Main class of Device

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/leveltwo/SmartHomeDevice.java
@@ -0,0 +1,10 @@
+package com.inheritance.leveltwo;
+
+public class SmartHomeDevice {
+	
+	    public static void main(String[] args) {
+
+	        Thermostat thermostat = new Thermostat("TH-110", true, 24);
+	        thermostat.displayStatus();
+	    }
+	}

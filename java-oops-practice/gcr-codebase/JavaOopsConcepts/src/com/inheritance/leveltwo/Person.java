64b76ba634312390f74929aee24ab0c818bbdd3b
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 20:23:44 +0530

    [Sanvi] : Added Person class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/leveltwo/Person.java
@@ -0,0 +1,15 @@
+package com.inheritance.leveltwo;
+
+public class Person {
+	
+	    String name;
+	    int age;
+
+	    //Constructor
+	    Person(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+	}
+
+

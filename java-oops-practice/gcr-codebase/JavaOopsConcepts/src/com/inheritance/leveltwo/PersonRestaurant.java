d8f4a3a3c549f643c20704b8eea3fd25fd891550
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 20:30:41 +0530

    [Sanvi] : Added PersonRestaurant class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/leveltwo/PersonRestaurant.java
@@ -0,0 +1,15 @@
+package com.inheritance.leveltwo;
+
+public class PersonRestaurant {
+	
+	    String name;
+	    int id;
+
+	    //Constructor
+	    PersonRestaurant(String name, int id) {
+	        this.name = name;
+	        this.id = id;
+	    }
+	}
+
+

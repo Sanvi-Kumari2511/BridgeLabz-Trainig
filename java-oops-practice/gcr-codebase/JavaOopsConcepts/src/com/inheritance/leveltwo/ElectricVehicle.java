be1b1d0099150aec5d2035a879b1e4f74ba6c701
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 20:37:50 +0530

    [Sanvi] : Added ElectricVehicle subclass of Vehicle class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/leveltwo/ElectricVehicle.java
@@ -0,0 +1,13 @@
+package com.inheritance.leveltwo;
+
+public class ElectricVehicle extends Vehicle {
+
+	    ElectricVehicle(int maxSpeed, String model) {
+	        super(maxSpeed, model);
+	    }
+
+	    void charge() {
+	        System.out.println("Charging the electric vehicle");
+	    }
+	}
+

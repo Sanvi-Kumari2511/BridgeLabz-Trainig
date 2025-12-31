b18d4f1dfb88f02bf1b9ee4a7a949346c13c3a3b
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 20:01:54 +0530

    [Sanvi] : Added Main class of Book

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/leveltwo/LibraryManagement.java
@@ -0,0 +1,17 @@
+package com.inheritance.leveltwo;
+
+public class LibraryManagement {
+	    public static void main(String[] args) {
+
+	        Author book1 = new Author(
+	                "The Hobbit",
+	                1937,
+	                "J.R.R. Tolkein",
+	                "Classic fantasy novel!"
+	        );
+
+	        book1.displayInfo();
+	    }
+	}
+
+

b5b0813202786e6d48b9d121d69c7e7e0aa32668
Sanvi <kumarisanvi2511@gmail.com>
2025-12-31 20:19:34 +0530

    [Sanvi] : Added SavingsAccount subclass of BankAccount

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/leveltwo/SavinsAccount.java
@@ -0,0 +1,19 @@
+package com.inheritance.leveltwo;
+
+ class SavingsAccount extends BankAccount{
+	
+	    double interestRate;
+
+	    SavingsAccount(String accountNumber, double balance, double interestRate) {
+	        super(accountNumber, balance);
+	        this.interestRate = interestRate;
+	    }
+
+	    //Override
+	    void displayAccountType() {
+	        System.out.println("Account Type: Savings Account");
+	        System.out.println("Interest Rate: " + interestRate + "%");
+	    }
+	}
+
+

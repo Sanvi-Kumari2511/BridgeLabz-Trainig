ec23db48ba348833a89a325cfbf9eb90b15095d4
Sanvi <kumarisanvi2511@gmail.com>
2026-01-01 15:39:42 +0530

    [Sanvi] : Added Book class

---
+++ java-oops-practice/scenario-based/oopsScenarioBased/src/day1Practice/Book.java
@@ -0,0 +1,12 @@
+package day1Practice;
+
+public class Book {
+  String title;
+  double price;
+  
+  //Constructor
+  Book (String title, double price){
+	  this.title = title;
+	  this.price = price;
+  }
+}
---
+++ java-oops-practice/scenario-based/oopsScenarioBased/src/day1Practice/Loan.java
@@ -0,0 +1,8 @@
+package day1Practice;
+
+public class Loan {
+
+	    public double calculateEMI(double amount, int months) {
+	        return amount / months;
+	    }
+}

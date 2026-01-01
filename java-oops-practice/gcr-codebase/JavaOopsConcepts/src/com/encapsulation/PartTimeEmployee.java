c6b7545e8b590a8d35f90240182f063635df5e8a
Sanvi <kumarisanvi2511@gmail.com>
2026-01-01 17:19:16 +0530

    [Sanvi] : Added PartTimeEmployee subclass of Employee

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/PartTimeEmployee.java
@@ -0,0 +1,19 @@
+package com.encapsulation;
+
+public class PartTimeEmployee extends Employee {
+	
+	    private int hoursWorked;
+	    private double hourlyRate;
+
+	    public PartTimeEmployee(int employeeId, String name, int hoursWorked, double hourlyRate) {
+	        super(employeeId, name, 0);
+	        this.hoursWorked = hoursWorked;
+	        this.hourlyRate = hourlyRate;
+	    }
+
+	    //Override
+	    public double calculateSalary() {
+	        return hoursWorked * hourlyRate;
+	    }
+	}
+

18dab74f9c5e90aa64268a45d0c7603a1f49d030
Sanvi <kumarisanvi2511@gmail.com>
2026-01-01 20:39:59 +0530

    [Sanvi] : Added Product clas

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/Product.java
@@ -0,0 +1,48 @@
+package com.encapsulation;
+
+    abstract class Product {
+	
+
+	    //Encapsulated fields
+	    private int productId;
+	    private String name;
+	    protected double price;
+
+	    //Constructor
+	    public Product(int productId, String name, double price) {
+	        this.productId = productId;
+	        this.name = name;
+	        this.price = price;
+	    }
+
+	    //Abstract method
+	    public abstract double calculateDiscount();
+
+	    //Encapsulation using getters and setters
+	    public int getProductId() {
+	        return productId;
+	    }
+
+	    public void setProductId(int productId) {
+	        this.productId = productId;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public double getPrice() {
+	        return price;
+	    }
+
+	    public void setPrice(double price) {
+	        if (price > 0) {
+	            this.price = price;
+	        }
+	    }
+	}
+

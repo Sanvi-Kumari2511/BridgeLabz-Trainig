3eae97554250b39af96ffa07e27bd37aa529bebc
Sanvi <kumarisanvi2511@gmail.com>
2026-01-01 20:45:45 +0530

    [Sanvi] : Added EcommercePlatform class

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/EcommercePlatform.java
@@ -0,0 +1,41 @@
+package com.encapsulation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EcommercePlatform {
+
+	    public static void calculateFinalPrice(List<Product> products) {
+
+	        for (Product product : products) {
+
+	            double tax = 0;
+
+	            if (product instanceof Taxable) {
+	                tax = ((Taxable) product).calculateTax();
+	            }
+
+	            double discount = product.calculateDiscount();
+	            double finalPrice = product.getPrice() + tax - discount;
+
+	            System.out.println("Product: " + product.getName());
+	            System.out.println("Base Price: " + product.getPrice());
+	            System.out.println("Discount: " + discount);
+	            System.out.println("Tax: " + tax);
+	            System.out.println("Final Price: " + finalPrice);
+	            System.out.println("-----------------------------");
+	        }
+	    }
+
+	    public static void main(String[] args) {
+
+	        List<Product> products = new ArrayList<>();
+
+	        products.add(new Electronics(1, "Mobile", 15000));
+	        products.add(new Clothing(2, "T-Shirt", 500));
+	        products.add(new Groceries(3, "Jacket", 1500));
+
+	        calculateFinalPrice(products);
+	    }
+ }
+
+

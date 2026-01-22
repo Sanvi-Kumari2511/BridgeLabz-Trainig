4cd4cc8e33621e28edabc195a88911e2de246cb0
Sanvi <kumarisanvi2511@gmail.com>
2026-01-22 20:33:30 +0530

    [Sanvi] : Added InvalidAgeException

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javacollections/exception/InvalidAgeException.java
@@ -0,0 +1,11 @@
+package com.javacollections.exception;
+import java.util.Scanner;
+
+public class InvalidAgeException extends Exception {
+	
+	    InvalidAgeException(String msg) {
+	        super(msg);
+	    }
+	}
+
+	

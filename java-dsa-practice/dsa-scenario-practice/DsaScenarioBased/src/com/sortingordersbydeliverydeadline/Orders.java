fb05a95b99b18e6b8fa4699e794eb2f4ed536143
Sanvi <kumarisanvi2511@gmail.com>
2026-01-22 20:21:33 +0530

    My changes

--- java-dsa-practice/dsa-scenario-practice/DsaScenarioBased/src/com/sortingordersbydeliverydeadline/Orders.java
+++ java-dsa-practice/dsa-scenario-practice/DsaScenarioBased/src/com/sortingordersbydeliverydeadline/Orders.java
@@ -4,10 +4,13 @@ public class Orders {
     String name;
     int deadline;
     
+    //Constructor to initialize
     Orders(String name, int deadline){
     	this.name=name;
     	this.deadline=deadline;
     }
+     
+    //This method converts the Orders object into a readable string
     public String toString() {
         return name + " → Day " + deadline;
     }

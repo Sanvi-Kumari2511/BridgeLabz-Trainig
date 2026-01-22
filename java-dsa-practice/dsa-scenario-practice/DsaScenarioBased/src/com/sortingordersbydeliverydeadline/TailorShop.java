367e07db1c737db7176577f745ff198012e15920
Sanvi <kumarisanvi2511@gmail.com>
2026-01-22 20:23:11 +0530

    My changes

--- java-dsa-practice/dsa-scenario-practice/DsaScenarioBased/src/com/sortingordersbydeliverydeadline/TailorShop.java
+++ java-dsa-practice/dsa-scenario-practice/DsaScenarioBased/src/com/sortingordersbydeliverydeadline/TailorShop.java
@@ -1,11 +1,14 @@
 package com.sortingordersbydeliverydeadline;
 
 public class TailorShop {
+	
+   //InsertionSort method
    public static void insertionSort(Orders[] orders) {
 	   for(int i = 1; i < orders.length; i++) {
 		   Orders temp = orders[i];
 		   int j = i - 1;
 		   
+		   //Check condition for insertion sort
 		   while(j >= 0 && orders[j].deadline > temp.deadline) {
 			     orders[j+1] = orders[j];
 			     j--;
@@ -16,6 +19,8 @@ public class TailorShop {
 		   
     }
      public static void main(String args []) {
+    	 
+    	 //Creating an array
     	 Orders orders [] = {
     			 
     			 new Orders ("Shirt", 5),
@@ -25,6 +30,7 @@ public class TailorShop {
     	 
     	 insertionSort(orders);
     	 
+    	 //Displaying sorted list
     	 for(int i = 0; i < orders.length; i++) {
     		 System.out.println(orders[i]);
     	 }

2599439e8cab08e00360a4d879aaa92407513673
Sanvi <kumarisanvi2511@gmail.com>
2026-01-25 17:21:43 +0530

    [Sanvi] : Added Author and AuthorAnnotationAccess and Book

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javareflection/intermediatelevel/Book.java
@@ -0,0 +1,8 @@
+package com.javareflection.intermediatelevel;
+
+@Author(name = "Robert Martin")
+public class Book {
+   public void display() {
+	   System.out.println("Book class loaded");
+   }
+}

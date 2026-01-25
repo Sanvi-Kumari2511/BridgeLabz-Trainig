f65f0aece64efd7056d20d2f023a81dcc268eff5
Sanvi <kumarisanvi2511@gmail.com>
2026-01-25 17:25:01 +0530

    [Sanvi] : Added MathOperations and MathOperationDynamicMethod

---
+++ java-collections-practice/gcr-codebase/JavaCollections/src/com/javareflection/intermediatelevel/MathOperations.java
@@ -0,0 +1,19 @@
+package com.javareflection.intermediatelevel;
+
+public class MathOperations {
+	
+	//Method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    //Method to subtract subtracts two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    //Method to multiplies two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}

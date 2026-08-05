d7d0d8ba5df25359e9fec40ca28e493e95ff46d5
Sanvi <kumarisanvi2511@gmail.com>
2026-08-05 16:49:15 +0530

    [Sanvi] : Added BubbleSort solution

---
+++ refresher-training/src/Day_04/BubbleSort_5.java
@@ -0,0 +1,34 @@
+package Day_04;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleSort_5 {
+    public static void countSwaps(List<Integer> a) {
+        int n = a.size();
+        int numSwaps = 0;
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j < n-1; j++){
+                if(a.get(j) > a.get(j+1)){
+                    int temp = a.get(j);
+                    a.set(j, a.get(j+1));
+                    a.set(j+1, temp);
+
+                    numSwaps++;
+                }
+            }
+
+        }
+
+        System.out.println("Array is sorted in " + numSwaps + " swaps. ");
+        System.out.println("First Element: " + a.get(0));
+        System.out.println("Last Element: " + a.get(n-1));
+
+    }
+    public static void main(String[] args) {
+        List<Integer> a = Arrays.asList(6,4,1);
+        countSwaps(a);
+
+    }
+}

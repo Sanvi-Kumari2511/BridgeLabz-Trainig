d5c57bcdf4a772a205d76bde7b370494eacff5cb
Sanvi <kumarisanvi2511@gmail.com>
2026-08-18 17:27:40 +0530

    [Sanvi] : Added Account solution

---
+++ refresher-training/src/Day_13_Section3/Account_05.java
@@ -0,0 +1,35 @@
+package Day_13_Section3;
+
+public class Account_05 {
+    private double balance;
+
+    Account_05(double balance){
+        this.balance = balance;
+    }
+    protected double getBalance(){
+        return balance;
+    }
+}
+
+class CurrentAccount extends Account_05 {
+
+    CurrentAccount(double balance){
+        super(balance);
+    }
+    void showBalance(){
+
+        //Error: balance has private access
+        // System.out.println(balance);
+
+        //Working: Protected method are accessible by its child class
+        System.out.println(getBalance());
+    }
+}
+
+class Main3{
+    public static void main(String[] args) {
+        CurrentAccount c = new CurrentAccount(30000);
+
+        c.showBalance();
+    }
+}

1615c0a256e6e894255614df0b00a3e9c514a502
Sanvi <kumarisanvi2511@gmail.com>
2026-08-18 17:25:55 +0530

    [Sanvi] : Added SalaryCalculator solution

---
+++ refresher-training/src/Day_13_Section3/SalaryCalculator_03.java
@@ -0,0 +1,23 @@
+package Day_13_Section3;
+
+public class SalaryCalculator_03 {
+    double calculateSalary(double basic){
+        return basic;
+    }
+
+    double calculateSalary(double basic, double bonus){
+        return basic + bonus;
+    }
+
+    double calculateSalary(double basic, double bonus, double deduction){
+        return basic + bonus - deduction;
+    }
+
+    public static void main(String[] args) {
+        SalaryCalculator_03 s = new SalaryCalculator_03();
+        System.out.println(s.calculateSalary(500));
+        System.out.println(s.calculateSalary(500, 200));
+        System.out.println(s.calculateSalary(500,300, 100));
+
+    }
+}

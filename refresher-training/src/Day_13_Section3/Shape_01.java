f6769322f897bde4e09652d3f205a64f6f6b57fe
Sanvi <kumarisanvi2511@gmail.com>
2026-08-18 17:23:44 +0530

    [Sanvi] : Added Shape solution

---
+++ refresher-training/src/Day_13_Section3/Shape_01.java
@@ -0,0 +1,63 @@
+package Day_13_Section3;
+
+public class Shape_01 {
+    double area(){
+        return 0;
+    }
+}
+class Circle extends Shape_01 {
+    double radius;
+
+    Circle(double radius){
+        this.radius = radius;
+    }
+    @Override
+    double area() {
+        return Math.PI*radius*radius;
+    }
+}
+
+class Rectangle extends Shape_01 {
+    double length;
+    double breadth;
+
+    Rectangle(double length, double breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    @Override
+    double area(){
+        return length*breadth;
+    }
+}
+
+class Triangle extends Shape_01 {
+    double base;
+    double height;
+
+    Triangle(double base,double height){
+        this.base = base;
+        this.height = height;
+   }
+
+    @Override
+    double area(){
+        return 0.5*base*height;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+        Shape_01[]  s = {
+                new Circle(5),
+                new Rectangle(10, 5),
+                new Triangle(8,4)
+        };
+
+        for(int i = 0; i < s.length; i++){
+
+            System.out.println(s[i].area());
+        }
+    }
+}

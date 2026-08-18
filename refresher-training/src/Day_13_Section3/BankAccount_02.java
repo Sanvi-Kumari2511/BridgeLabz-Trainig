4c78850589fe91c97f477b63407289dec8f575fe
Sanvi <kumarisanvi2511@gmail.com>
2026-08-18 17:25:01 +0530

    [Sanvi] : Added BankAccount solution

---
+++ refresher-training/src/Day_13_Section3/BankAccount_02.java
@@ -0,0 +1,39 @@
+package Day_13_Section3;
+
+public class BankAccount_02 {
+    String accountHolder;
+    double balance;
+    BankAccount_02(){
+        this("Unknown", 0);
+    }
+
+    BankAccount_02(String accountHolder){
+        this(accountHolder, 0);
+    }
+
+    BankAccount_02(String accountHolder, double balance) {
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    void display(){
+        System.out.println(accountHolder);
+        System.out.println(balance);
+    }
+}
+
+class SavingsAccount extends BankAccount_02{
+    double interestRate;
+    SavingsAccount(String accountHolder, double balance, double interestRate){
+        super(accountHolder, balance);
+        this.interestRate =  interestRate;
+    }
+}
+
+ class Main1{
+     public static void main(String[] args) {
+         SavingsAccount s = new SavingsAccount("Sanvi",100000, 5);
+         s.display();
+         System.out.println(s.interestRate + "%");
+     }
+}

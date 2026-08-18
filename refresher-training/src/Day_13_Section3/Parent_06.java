643d94b0a70c260efe6cc548f86fe7a572ec9264
Sanvi <kumarisanvi2511@gmail.com>
2026-08-18 17:28:23 +0530

    [Sanvi] : Added Parent solution

---
+++ refresher-training/src/Day_13_Section3/Parent_06.java
@@ -0,0 +1,23 @@
+package Day_13_Section3;
+
+public class Parent_06 {
+    static void show(){
+        System.out.println("Parent");
+    }
+}
+
+class Child extends Parent_06 {
+    static void show(){
+        System.out.println("Child");
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Parent_06 p = new Child();
+
+        //Reference type is Parent and method is static so reference type decides which method to call
+        p.show(); //Parent method
+
+    }
+}

2da4330022ab64f763232b853b596d06b17f79eb
Sanvi <kumarisanvi2511@gmail.com>
2026-08-18 17:30:15 +0530

    [Sanvi] : Added TrafficLight solution

---
+++ refresher-training/src/Day_13_Section3/TrafficLight_07.java
@@ -0,0 +1,27 @@
+package Day_13_Section3;
+
+public enum TrafficLight_07 {
+    RED(30),
+    YELLOW(5),
+    GREEN(25);
+
+    private int duration;
+
+    TrafficLight_07(int duration) {
+        this.duration = duration;
+    }
+
+    int getDuration() {
+        return duration;
+    }
+}
+
+class Main5{
+    public static void main(String[] args) {
+        TrafficLight_07[] l = TrafficLight_07.values();
+
+        for(int i = 0 ; i < l.length; i++){
+            System.out.println(l[i] + " " + l[i].getDuration() + " seconds");
+        }
+    }
+}

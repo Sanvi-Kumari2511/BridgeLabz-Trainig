0e7df6d28ca9644e0401ffa05c4f804333030829
Sanvi <kumarisanvi2511@gmail.com>
2026-08-20 14:25:36 +0530

    [Sanvi] : Added AdvancedArithmetic solution

---
+++ refresher-training/src/Day_15/AdvancedArithmetic_01.java
@@ -0,0 +1,31 @@
+package Day_15;
+
+interface AdvancedArithmetic_01 {
+    int divisor_sum(int n);
+}
+
+//Write your code here
+class MyCalculator implements AdvancedArithmetic_01 {
+    public int divisor_sum(int n){
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                sum += i;
+            }
+        }
+        return sum;
+
+    }
+}
+
+class Solution4{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+
+        System.out.println("I implemented: AdvancedArithmetic");
+        int n = 6;
+        System.out.println(my_calculator.divisor_sum(n) + " ");
+
+    }
+
+}
